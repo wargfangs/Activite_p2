5548c3867e012e7e4e789de07fe2c0c5ef1d57e2
wargfangs <n.tchao@hotmail.fr>
2017-05-10 20:03:33 +0200

    Ajout du script js et du txt symfony

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function toggle(elemID) {
+var elem = document.getElementById(elemID);
+
+if (elem.style.display === 'block') {
+elem.style.display = 'none';	
+} else {
+elem.style.display = 'block';	
+}
+}
